b9181a14082896eeff06eeac32672b44a952647e
s2j55 <s2jeongyang@gmail.com>
2021-01-04 11:03:55 +0900

    :tada: first typescript

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("hello");
+const sayHi = (name:string, age:number, gender:string) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi("Sejeong", 28, "female");
+
+  export {};
