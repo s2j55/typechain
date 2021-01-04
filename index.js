80f953090cab2c6d760ce7a3c64df32b8c6d1a80
s2j55 <s2jeongyang@gmail.com>
2021-01-04 13:54:36 +0900

    :art: blockchain

--- index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
-};
-sayHi("Sejeong", 28, "female");
-//# sourceMappingURL=index.js.map
--- index.ts
+++
@@ -1,7 +0,0 @@
-const sayHi = (name:string, age:number, gender:string) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
-};
-
-sayHi("Sejeong", 28, "female");
-
-  export {};
